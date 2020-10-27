0af1fa63d9631e04aba08e35899db8c8a3484036
gsaidov <gulomjonsaidov@gmail.com>
2020-10-26 22:26:55 -0700

    navbar

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+    $("#loginButton").click(function() {
+        $("#loginModal").modal('show');
+    });
+    $("#signUpButton").click(function() {
+        $("#signUpModal").modal("show");
+    });
+    $("#signUpModal").on('show.bs.modal', function (e) {
+        $("#loginModal").modal("hide");
+    });
+    $("#loginModal").on('show.bs.modal', function (e) {
+        $("#signUpModal").modal("hide");
+    });
+});
